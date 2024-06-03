4dbb7646891739faa1d30142243069070f3d7145
MMorlaye <smorlaye313@gmail.com>
2024-06-03 06:12:40 +0200

    Latest home page settings added

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleCardClick(element) {
+    // Action au clic, par exemple redirection
+    alert('Card clicked!');
+    // Vous pouvez ajouter une redirection ou une autre action ici
+    // window.location.href = 'newpage.html';
+}
